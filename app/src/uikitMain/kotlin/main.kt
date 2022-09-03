ba3b44ed8b2f61140a5c4f539732496497a988dd
yt8492 <tmymyut2000@gmail.com>
2022-09-03 20:01:52 +0900

    fix

--- app/src/uikitMain/kotlin/main.kt
+++ app/src/uikitMain/kotlin/main.kt
@@ -28,7 +28,7 @@ class SkikoAppDelegate : UIResponder, UIApplicationDelegateProtocol {
     }
 
     override fun application(application: UIApplication, didFinishLaunchingWithOptions: Map<Any?, *>?): Boolean {
-        window = UIWindow(frame = UIScreen.mainScreen.bounds).apply {
+        window = UIWindow(frame = UIScreen.mainScreen.applicationFrame).apply {
             rootViewController = Application("Chat") {
                 Root()
             }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.3.0-beta05" apply false
-    id("com.android.library") version "7.3.0-beta05" apply false
+    id("com.android.application") version "7.3.0-rc01" apply false
+    id("com.android.library") version "7.3.0-rc01" apply false
     kotlin("multiplatform") version "1.6.21" apply false
     id("org.jetbrains.compose") version "1.2.0-alpha01-dev725" apply false
 }
